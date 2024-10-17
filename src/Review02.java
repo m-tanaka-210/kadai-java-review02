506ec2fc85eb6a103a63f68c187c58dcf01600bc
m-tanaka-210 <writexbrown@yahoo.co.jp>
2024-10-18 01:07:58 +0900

    Lwsson4課題「FizzBuzz問題のプログラムを作成しよう」提出

---
+++ src/Review02.java
@@ -0,0 +1,22 @@
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        // iの値を１から100までの連続した整数、1ずつカウントアップして繰り返す
+        for (int i = 1; i <= 100; i++) {
+            // iの値が3で割り切れ、なおかつ5で割り切れるとき
+            if (i % 3 == 0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                //　iの値が3で割り切れるときFizzと表示する
+                System.out.println("Fizz!");
+            } else if (i % 5 == 0) {
+                // iの値が5で割り切れるときBuzzと表示する
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
